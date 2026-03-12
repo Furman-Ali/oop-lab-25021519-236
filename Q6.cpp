7346c118142969f0d34cb423525e40c285aef407
Furman-Ali <furmanali023@gmail.com>
2026-03-12 23:01:37 +0500

    Q6.cpp done

---
+++ Q6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Flashlight {
+private:
+    int id;
+public:
+    Flashlight(int i) : id(i) {
+        cout << "Flashlight " << id << " created." << endl;
+    }
+    ~Flashlight() {
+        cout << "Flashlight " << id << " destroyed." << endl;
+    }
+};
+
+void useLight() {
+    Flashlight f2(2);
+}
+
+int main() {
+    Flashlight f1(1);
+    {
+        Flashlight f3(3);
+    }
+    useLight();
+    Flashlight f4(4);
+    return 0;
+}
